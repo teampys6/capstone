025c80fc2471929ed911b44af2f3850d9f092bff
Arthur Luiz <flashgames502@gmail.com>
2023-07-07 19:39:25 -0300

    Create script.js
    Adicionando a animação de scrollreveal pelo JavaScript.

---
+++ script.js
@@ -0,0 +1,59 @@
+
+window.sr = ScrollReveal({ reset: true })
+
+sr.reveal('.mountain', { 
+    duration: 1000,
+})
+
+sr.reveal('.title-text', {
+    rotate: {x: 0, y: 180, z: 0},
+    duration: 1000
+})
+
+sr.reveal('.icon-profile', {
+    duration: 1500
+})
+
+sr.reveal('.title-intro', {
+    duration: 1500
+})
+
+sr.reveal('.intro', {
+    duration: 1500
+})
+
+sr.reveal('.skills', {
+    duration: 1500
+})
+
+sr.reveal('.project', {
+    duration: 1500
+})
+
+sr.reveal('.image-perfum', {
+    delay: 200,
+    easing: "ease-out",
+    interval: 200,
+    scale: 1.2
+})
+
+sr.reveal('.perfum-about', {
+    delay: 200,
+    easing: "ease-out",
+    interval: 200,
+    scale: 1.2
+})
+
+sr.reveal('.image-grid', {
+    delay: 200,
+    easing: "ease-out",
+    interval: 200,
+    scale: 1.2
+})
+
+sr.reveal('.grid-about', {
+    delay: 200,
+    easing: "ease-out",
+    interval: 200,
+    scale: 1.2
+})
